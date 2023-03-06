d69dfab6a3cb3dff05daa6b0d20a05e086024861
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-03-06 17:06:59 +0530

    added person gen testing

---
+++ src/main/java/org/specialiststeak/peoplegenerator/Testing.java
@@ -0,0 +1,19 @@
+package org.specialiststeak.peoplegenerator;
+
+import com.opencsv.exceptions.CsvValidationException;
+import org.specialiststeak.peoplegenerator.person.peoplelist.Person;
+
+import java.io.IOException;
+
+import static org.specialiststeak.peoplegenerator.person.utils.Utils.startup;
+
+public class Testing {
+    public static void main(String[] args) throws CsvValidationException, IOException {
+        startup();
+        System.out.println(new Person());
+        long start = System.nanoTime();
+        new Person();
+        long end = System.nanoTime();
+        System.out.println("Took " + (end - start) + "ns");
+    }
+}
